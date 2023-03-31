1dbb8068bf06716c03d618b1636cbd212ca93aab
Anand Jose <anand.jose@trenser.com>
2023-03-31 13:16:14 +0530

    dfd

--- server.js
+++ server.js
@@ -1,11 +1,51 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Hello World AWS!')
-})
-https://runkit.com/
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+
+app.get('/chat', (req, res) => {
+    res.sendFile(__dirname+"/public_1/board.html")
+  })
+// app.listen(3002, () => {
+//    console.log(`Example app listening on port 3002`)
+// })
+
+
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3001",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+//   socket.on("join_room", (data) => {
+//     socket.join(data);
+//     console.log(`User with ID: ${socket.id} joined room: ${data}`);
+//   });
+
+  socket.on("get_message", (from,data) => {
+    console.log(from,data)
+    // socket.broadcast.emit("receive_message", from,data);
+    io.emit("receive_message", from,data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
+
+
+
+
+server.listen(3001, () => {
+  console.log("SERVER RUNNING");
+});
