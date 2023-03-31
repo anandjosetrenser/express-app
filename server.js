8ed687035bb07ccbd06570e862e939187a3d41a1
Anand Jose <anand.jose@trenser.com>
2023-03-31 14:14:08 +0530

    fgfg

--- server.js
+++ server.js
@@ -9,38 +9,38 @@ app.use(cors());
 app.get('/', (req, res) => {
     res.sendFile(__dirname+"/public_1/board.html")
   })
-app.listen(3002, () => {
-   console.log(`Example app listening on port 3002`)
-})
+// app.listen(3002, () => {
+//    console.log(`Example app listening on port 3002`)
+// })
 
 
 
-// const server = http.createServer(app);
+const server = http.createServer(app);
 
-// const io = new Server(server, {
-//   cors: {
-//     origin: "http://localhost:3001",
-//     methods: ["GET", "POST"],
-//   },
-// });
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3001",
+    methods: ["GET", "POST"],
+  },
+});
 
-// io.on("connection", (socket) => {
-//   console.log(`User Connected: ${socket.id}`);
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
 
-//   socket.on("get_message", (from,data) => {
-//     console.log(from,data)
-//     // socket.broadcast.emit("receive_message", from,data);
-//     io.emit("receive_message", from,data);
-//   });
+  socket.on("get_message", (from,data) => {
+    console.log(from,data)
+    // socket.broadcast.emit("receive_message", from,data);
+    io.emit("receive_message", from,data);
+  });
 
-//   socket.on("disconnect", () => {
-//     console.log("User Disconnected", socket.id);
-//   });
-// });
-
-
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
 
 
-// server.listen(3001, () => {
-//   console.log("SERVER RUNNING");
-// });
+
+
+server.listen(3000, () => {
+  console.log("SERVER RUNNING");
+});
